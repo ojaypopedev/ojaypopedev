ce14c2c2cf46a60b9b4a8346cd78913853cb706f
Oliver <44278867+ojaypopedev@users.noreply.github.com>
2020-03-26 22:16:52 +0000

    Initial

---
+++ Staying Mythical 2/Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,395 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mythical;
+
+public class WorldGenerator : MonoBehaviour
+{
+ 
+  
+    [SerializeField] private Vector2Int gridSize = new Vector2Int(2, 2);
+    private Vector2Int oldGridSize;
+    [SerializeField]  GroundTile[,] GroundTiles;
+   
+
+    [ContextMenu("GenerateGrid")]
+    public void GenerateGrid()
+    {
+        RemoveOldGrid();
+        CreateNewGrid();
+        CreateBase();
+        CreateEnvironment();
+
+       
+    }
+
+    public void GenerateRocks()
+    {
+
+    }
+
+
+    void RemoveOldGrid()
+    {
+        if(GroundTiles != null)
+            
+        for (int x = 0; x < oldGridSize.x; x++)
+        {
+            for (int y = 0; y < oldGridSize.y; y++)
+            {
+                if (GroundTiles[x, y] != null)
+                {
+                    GroundTiles[x, y].DestoyTile();
+                       
+                }
+
+            }
+        }
+
+
+        foreach (var item in GetComponentsInChildren<Transform>())
+        {
+            if(item != this.transform)
+            {
+                Destroy(item);
+            }
+        }
+    }
+    void CreateNewGrid()
+    {
+
+        GroundTiles = new GroundTile[gridSize.x, gridSize.y];
+
+
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                GroundTiles[x, y] = new GroundTile(new Vector3(x * GroundTile.TileSize, 0, y * GroundTile.TileSize), transform);
+                GroundTiles[x, y].EnvironmentName = x + "_" + y;
+                
+
+               
+            }
+        }
+        oldGridSize = gridSize;
+    }
+
+    void CreateBase()
+    {
+        Vector2Int baseTile = GroundTile.randomTile(gridSize);
+        GroundTiles[baseTile.x, baseTile.y].addObstacle(Environment.Obsctacles.Base);
+    }
+
+    void CreateEnvironment()
+    {
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    GroundTiles[x, y].addObstacle(Environment.Obsctacles.Rock);
+                    GroundTiles[x, y].addObstacle(Environment.Obsctacles.Tree);
+                }
+
+            }
+        }
+    }
+
+}
+
+
+namespace Mythical
+{
+
+
+    using UnityEngine;
+
+   
+    [System.Serializable]
+    public class GroundTile : Environment
+    {
+
+       public static Vector2Int randomTile(Vector2Int gridSize)
+       {
+           return new Vector2Int(Mathf.RoundToInt(Random.Range(0, gridSize.x)), Mathf.RoundToInt(Random.Range(0, gridSize.y)));
+
+       }
+
+        public static readonly float TileSize = 64;
+        public static readonly float ObstacleMargin = 5;
+
+        private List<Obstacle> _obsctalces = new List<Obstacle>();
+        public Obstacle[] Obtacles { get { return _obsctalces.ToArray(); } }
+        public void addObstacle(Obsctacles type, Vector2 position)
+        {
+            switch (type)
+            {
+                case Obsctacles.Rock:
+                    _obsctalces.Add(new Rock(position,this));
+                    break;
+                case Obsctacles.Tree:
+                    _obsctalces.Add(new Tree(position,this));
+                    break;
+                case Obsctacles.Base:
+                    _obsctalces.Add(new Base(position,this));
+                    break;
+                default:
+                    break;
+            }
+            
+
+        }
+        public void addObstacle(Obsctacles type)
+        {
+            switch (type)
+            {
+                case Obsctacles.Rock:
+                    _obsctalces.Add(new Rock(this));
+                    break;
+                case Obsctacles.Tree:
+                    _obsctalces.Add(new Tree(this));
+                    break;
+                case Obsctacles.Base:
+                    _obsctalces.Add(new Base(this));
+                    break;
+                case Obsctacles.Explorer:
+                    _obsctalces.Add(new Explorer(this));
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public GroundTile()
+        {
+
+        }
+
+        public GroundTile(Vector3 position,Transform parent)
+        {
+           
+            
+            EnvironmentObject = Object.Instantiate((GameObject)Resources.Load("EnvironmentObjects/GroundTile"));
+            EnvironmentTransform.parent = parent;
+            EnvironmentTransform.position = position;
+
+        }
+
+        public GroundTile(Vector3 position)
+        {
+
+            EnvironmentObject = Object.Instantiate((GameObject)GameObjectReference.GroundTile);
+            EnvironmentTransform.parent = null;
+            EnvironmentTransform.position = position;
+
+        }
+
+        public void DestoyTile()
+        {
+            Object.DestroyImmediate(EnvironmentObject);
+           
+        }
+    }
+
+    [System.Serializable]
+    public class Environment
+    {
+        private string name;
+        public string EnvironmentName { get { return name; } set { name = EnvironmentName; EnvironmentObject.name = name; } }
+        public enum Obsctacles {Rock, Tree, Base, Explorer};
+
+        public GameObject EnvironmentObject;
+
+        public Transform EnvironmentTransform { get { return EnvironmentObject.transform; } }
+
+    }
+
+    [System.Serializable]
+    public class Obstacle : Environment
+    {
+        public GroundTile GroundParent;
+
+        public Vector2 ObstaclePosition;
+        private Vector3 SpawnPos()
+        {
+            return new Vector3(ObstaclePosition.x, 0, ObstaclePosition.y);
+        }
+
+        public Obsctacles obstacleType;
+        public Obsctacles ObstacleType { get { return obstacleType; } }
+        public void CreateObstacle()
+        {
+            GameObject _toSpawn;
+
+            switch (obstacleType)
+            {
+                case Obsctacles.Rock:
+                    _toSpawn = GameObjectReference.Rock;
+                    break;
+                case Obsctacles.Tree:
+                    _toSpawn = GameObjectReference.Tree;
+                    break;
+                case Obsctacles.Base:
+                    _toSpawn = GameObjectReference.Base;
+                    break;
+                default:
+                    _toSpawn = GameObjectReference.Rock;
+                    break;
+            }
+
+            Debug.Log(GroundParent.EnvironmentTransform);
+            EnvironmentObject = GameObject.Instantiate(_toSpawn, GroundParent.EnvironmentTransform.position + SpawnPos(), Quaternion.identity, GroundParent.EnvironmentTransform);
+       
+
+        }
+
+        public static bool MinObstacleDistance(Obstacle toCheck, Vector3 position, Obstacle existingObstacle, float min)
+        {
+            if(toCheck.EnvironmentObject && existingObstacle.EnvironmentObject)
+            {
+                Vector3 sizeA = toCheck.EnvironmentObject.GetComponent<MeshFilter>().sharedMesh.bounds.extents;
+                sizeA.y = 0;
+                Vector3 sizeB = existingObstacle.EnvironmentObject.GetComponent<MeshFilter>().sharedMesh.bounds.extents;
+                sizeB.y = 0;
+
+                return Vector3.Distance(existingObstacle.ObstaclePosition, position) > min + sizeA.magnitude + sizeB.magnitude;
+            }
+            else
+            {
+                return false;
+            }
+        }
+       
+        public void FindSuitablePosition(float size)
+        {
+            ObstaclePosition = new Vector2(Random.Range(0+GroundTile.ObstacleMargin, GroundTile.TileSize- GroundTile.ObstacleMargin), Random.Range(0+GroundTile.ObstacleMargin, GroundTile.TileSize- GroundTile.ObstacleMargin));
+
+            bool passed = false;
+            int iterations = 0;
+            while (passed == false && iterations < 100)
+            {
+                if (iterations == 99) Debug.LogWarning("Failed to find pos");
+                passed = true;
+                iterations++;
+
+                foreach (var item in GroundParent.Obtacles)
+                {
+                    if (!MinObstacleDistance(this, ObstaclePosition, item, size))
+                    {
+                        passed = false;
+                        ObstaclePosition = new Vector2(Random.Range(0, GroundTile.TileSize), Random.Range(0, GroundTile.TileSize));
+                    }
+
+                }
+            }
+
+            EnvironmentTransform.position = GroundParent.EnvironmentTransform.position + SpawnPos();
+
+        }
+
+    }
+
+    [System.Serializable]
+    public class Rock: Obstacle
+    {      
+        public Rock(Vector2 Pos, GroundTile parent)
+        {
+            obstacleType = Obsctacles.Rock;
+            ObstaclePosition = Pos;
+            GroundParent = parent;
+            CreateObstacle();
+        }
+        public Rock(GroundTile parent)
+        {
+            
+            obstacleType = Obsctacles.Rock;      
+            GroundParent = parent;
+            CreateObstacle();
+            FindSuitablePosition(3);
+        }
+
+    }
+
+    [System.Serializable]
+    public class Base : Obstacle
+    {
+        public Base(Vector2 Pos, GroundTile parent)
+        {
+            obstacleType = Obsctacles.Base;
+            ObstaclePosition = Pos;
+            GroundParent = parent;
+            CreateObstacle();
+        }
+        public Base(GroundTile parent)
+        {
+            
+
+            obstacleType = Obsctacles.Base;
+            ObstaclePosition = new Vector2(Random.Range(0, GroundTile.TileSize), Random.Range(0, GroundTile.TileSize));
+            GroundParent = parent;
+            CreateObstacle();
+            FindSuitablePosition(8);
+        }
+
+    }
+
+    [System.Serializable]
+    public class Tree:Obstacle
+    {
+        public Tree(Vector2 Pos, GroundTile parent)
+        {
+            
+            obstacleType = Obsctacles.Tree;
+            ObstaclePosition = Pos;
+            GroundParent = parent;
+            CreateObstacle();
+        }
+        public Tree(GroundTile parent)
+        {
+          
+            obstacleType = Obsctacles.Tree;      
+            GroundParent = parent;
+            CreateObstacle();
+            FindSuitablePosition(5);
+        }
+
+
+    }
+
+    [System.Serializable]
+    public class Explorer : Obstacle
+    {
+        public Explorer(Vector2 Pos, GroundTile parent)
+        {
+
+            obstacleType = Obsctacles.Tree;
+            ObstaclePosition = Pos;
+            GroundParent = parent;
+            CreateObstacle();
+        }
+        public Explorer(GroundTile parent)
+        {
+            ObstaclePosition = new Vector2(Random.Range(0, GroundTile.TileSize), Random.Range(0, GroundTile.TileSize));
+
+            obstacleType = Obsctacles.Tree;
+
+            GroundParent = parent;
+            CreateObstacle();
+            FindSuitablePosition(3);
+        }
+    }
+
+    [System.Serializable]
+    public static class GameObjectReference
+    {
+        public static GameObject GroundTile = (GameObject)Resources.Load("EnvironmentObjects/GroundTile");
+        public static GameObject Rock = (GameObject)Resources.Load("EnvironmentObjects/Rock");
+        public static GameObject Tree = (GameObject)Resources.Load("EnvironmentObjects/Tree");
+        public static GameObject Base = (GameObject)Resources.Load("EnvironmentObjects/Base");
+        public static GameObject Explorer = (GameObject)Resources.Load("EnvironmentObjects/Explorer");
+    }
+}
+
---
+++ Staying Mythical 2/Assets/playerController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    [SerializeField] float lookamount = 0;
+    float[] lookMax = { -30, 80 };
+    Transform head;
+    Rigidbody rb;
+    float walkSpeed = 4;
+    float runSpeed = 7;
+    List<Collision>collisions = new List<Collision>();
+    bool isGrounded = false;
+  
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        head = GetComponentInChildren<Camera>().transform;
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+        Collide();
+        Move();
+       
+       
+
+    }
+
+    void Look()
+    {
+        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+
+
+        lookamount -= Input.GetAxis("Mouse Y");
+
+        if (lookamount > lookMax[1] || lookamount < lookMax[0])
+        {
+            lookamount -= Input.GetAxis("Mouse Y");
+        }
+        else
+        {
+            head.Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
+        }
+
+    }
+    void Move()
+    {
+
+        Vector3 movement = Vector3.zero;
+
+        bool wDown = Input.GetKey(KeyCode.W);
+        bool sDown = Input.GetKey(KeyCode.S);
+        bool dDown = Input.GetKey(KeyCode.D);
+        bool aDown = Input.GetKey(KeyCode.A);
+        bool shiftDown = Input.GetKey(KeyCode.LeftShift);
+        bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+
+        float speed = shiftDown ? runSpeed : walkSpeed;
+
+        if(wDown)
+        {
+            movement += transform.forward * speed;
+        }
+
+        if (sDown)
+        {
+            movement -= transform.forward * speed;
+        }
+
+        if(dDown)
+        {
+            movement += transform.right * speed;
+        }
+
+        if(aDown)
+        {
+            movement -= transform.right * speed;
+        }
+
+        if(spaceDown)
+        {
+            if(isGrounded)
+            {
+                movement += Vector3.up * 10;
+            }
+            else
+            {
+                Debug.Log("Not Grounded");
+            }
+        }
+
+        
+        movement.y += rb.velocity.y;
+       
+        rb.velocity = movement;
+
+    }
+
+    void Collide()
+    {
+        foreach (var col in collisions)
+        {
+            if(col.gameObject.tag == "Ground")
+            {
+                isGrounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+      if(!collisions.Contains(collision))
+        {
+            collisions.Add(collision);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collisions.Contains(collision))
+        {
+            collisions.Remove(collision);
+        }
+    }
+}
